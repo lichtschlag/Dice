8395ee146d9b1cad61f35fc1dd6395d74ba65afa
Leonhard Lichtschlag <leonhard.lichtschlag@rwth-aachen.de>
2012-02-28 16:37:17 +0100

    slowed down time, added motions with an emphasis on shits in the posture

--- DiceMobile/DiceMobile/DMViewController.mm
+++ DiceMobile/DiceMobile/DMViewController.mm
@@ -7,6 +7,7 @@
 //
 
 #import "DMViewController.h"
+#import <CoreMotion/CoreMotion.h>
 #import "btBulletDynamicsCommon.h"
 
 
@@ -66,7 +67,7 @@ GLfloat gCubeVertexData[216] =
 {
 	float _rotation;
 	
-	GLuint _vertexArray;
+//	GLuint _vertexArray;
 	GLuint _vertexBuffer;
 	
 	// Physics
@@ -78,11 +79,15 @@ GLfloat gCubeVertexData[216] =
 	btSequentialImpulseConstraintSolver*	sConstraintSolver;
 	
 	btAlignedObjectArray<btRigidBody*>		sBoxBodies;
+	
+	double gravity[3];
+	BOOL firstAccelerometerData;
 }
 
 @property (strong, nonatomic) EAGLContext *context;
 @property (strong, nonatomic) GLKBaseEffect *effect;
 @property int diceNumber;
+@property (strong) CMMotionManager* motionManager;
 
 - (void) setupGL;
 - (void) setupScene;
@@ -118,16 +123,24 @@ GLfloat gCubeVertexData[216] =
 	[self setupGL];
 	[self setupBullet];
 	[self setupScene];
+	
+	self.motionManager = [[CMMotionManager alloc] init]; // motionManager is an instance variable
+	self.motionManager.accelerometerUpdateInterval = 0.16; // 60Hz
+	[self.motionManager startAccelerometerUpdates];
+	memset(gravity, 0, sizeof(gravity));
+	firstAccelerometerData = YES;
 }
 
 
 - (void) viewDidUnload
 {    
-    [super viewDidUnload];
+	[super viewDidUnload];
     
-    [self tearDownGL];
-    [self tearDownBullet];    
-    [self tearDownScene];
+	[self.motionManager stopAccelerometerUpdates];
+	
+	[self tearDownGL];
+	[self tearDownBullet];    
+	[self tearDownScene];
 }
 
 
@@ -152,8 +165,10 @@ GLfloat gCubeVertexData[216] =
 	// load mesh for one dice
 	[EAGLContext setCurrentContext:self.context];
 
-	glGenVertexArraysOES(1, &_vertexArray);
-	glBindVertexArrayOES(_vertexArray);
+	// load mesh for one dice
+
+//	glGenVertexArraysOES(1, &_vertexArray);
+//	glBindVertexArrayOES(_vertexArray);
     
     glGenBuffers(1, &_vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
@@ -164,12 +179,19 @@ GLfloat gCubeVertexData[216] =
     glEnableVertexAttribArray(GLKVertexAttribNormal);
     glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 24, BUFFER_OFFSET(12));
     
-    glBindVertexArrayOES(0);
+//	glBindVertexArrayOES(0);
 	
 	// create a simple effect for the dice
 	self.effect = [[GLKBaseEffect alloc] init];
 	self.effect.light0.enabled			= GL_TRUE;
 	self.effect.light0.diffuseColor	= GLKVector4Make(1.0f, 0.4f, 0.4f, 1.0f);
+	// compute projection matix
+    float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f),	// FoV
+															aspect,									// Sceen aspect
+															0.1f, 100.0f);							// Near/far plane
+    self.effect.transform.projectionMatrix = projectionMatrix;
+
 	
 	// -----------------------------------------------------------------------------------------------------------
 	sDynamicsWorld->setGravity( btVector3(0, -10 ,0) );
@@ -191,7 +213,8 @@ GLfloat gCubeVertexData[216] =
 		
 		btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
 		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, worldBoxSideShape, localInertia);
-		
+		rbInfo.m_restitution	= 0.9;
+
 		btRigidBody* sFloorPlaneBody = new btRigidBody(rbInfo);
 		
 		// add the body to the dynamics world
@@ -203,7 +226,7 @@ GLfloat gCubeVertexData[216] =
 	{
 		btCollisionShape* boxShape = new btBoxShape( btVector3(1, 1, 1) );
 		
-		btScalar mass = 1.0;		// positive mass means dynamic/moving object
+		btScalar mass = 0.20;		// positive mass means dynamic/moving object
 		btVector3 localInertia(0, 0, 0);
 		boxShape->calculateLocalInertia(mass, localInertia);
 		
@@ -215,7 +238,7 @@ GLfloat gCubeVertexData[216] =
 
 		btDefaultMotionState* myMotionState = new btDefaultMotionState(objectTransform);
 		btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, boxShape, localInertia);
-		rbInfo.m_restitution	= 3;
+		rbInfo.m_restitution	= 1.0;
 		btRigidBody* boxBody	= new btRigidBody(rbInfo);
 		
 		sBoxBodies.push_back(boxBody);
@@ -253,7 +276,7 @@ GLfloat gCubeVertexData[216] =
 	// release buffers
     [EAGLContext setCurrentContext:self.context];
     glDeleteBuffers(1, &_vertexBuffer);
-    glDeleteVertexArraysOES(1, &_vertexArray);
+//    glDeleteVertexArraysOES(1, &_vertexArray);
     
 	// release effects
     self.effect = nil;
@@ -300,19 +323,36 @@ GLfloat gCubeVertexData[216] =
 // Update the world state
 - (void) update
 {
-	// compute projection matix
-    float aspect = fabsf(self.view.bounds.size.width / self.view.bounds.size.height);
-    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f),	// FoV
-															aspect,									// Sceen aspect
-															0.1f, 100.0f);							// Near/far plane
-    
-    self.effect.transform.projectionMatrix = projectionMatrix;
-    	
-	// move physics forward
-	float frameRate = 1 / self.timeSinceLastUpdate;
-	//	NSLog(@"%s %f %f %d", __PRETTY_FUNCTION__, frameRate, self.timeSinceLastUpdate, self.preferredFramesPerSecond);
+	// get two vetors: gravity and current motion
+	float alpha = 0.1;
+	double motion[3] = {0,0,0};
+	CMAccelerometerData *newestAccel = self.motionManager.accelerometerData;
+	if (firstAccelerometerData)
+	{
+		gravity[0] = newestAccel.acceleration.x;
+		gravity[1] = newestAccel.acceleration.y;
+		gravity[2] = newestAccel.acceleration.z;
+		firstAccelerometerData = NO;
+	}
+	else
+	{
+		gravity[0] = gravity[0] * (1.0-alpha) + newestAccel.acceleration.x * alpha;
+		gravity[1] = gravity[1] * (1.0-alpha) + newestAccel.acceleration.y * alpha;
+		gravity[2] = gravity[2] * (1.0-alpha) + newestAccel.acceleration.z * alpha;
+	}
+	motion[0]	= newestAccel.acceleration.x - gravity[0];
+	motion[1]	= newestAccel.acceleration.y - gravity[1];
+	motion[2]	= newestAccel.acceleration.z - gravity[2];
+	
+	float gScaling = 0.0;			// stil should fall down as if the device was helt in portrait
+	float mScaling = 300.0;		// emphasize changes a bit more, so that we notice it more
+	sDynamicsWorld->setGravity( btVector3(gScaling * gravity[0] + mScaling *motion[0], 
+										    gScaling * gravity[1] + mScaling *motion[1] - 10.0,
+										    gScaling * gravity[2] + mScaling *motion[2]));
 
-	sDynamicsWorld->stepSimulation(frameRate, 2); // allow 2 steps if our graphics are lagging
+	// move physics forward
+	float updateRate = 1 / self.timeSinceLastUpdate;
+	sDynamicsWorld->stepSimulation(updateRate, 2, 1.0/150.0); // allow 2 steps if our graphics are lagging
 	
 	// for debugging: print the timing on the 10th frame
 	static int i = 0;
@@ -321,7 +361,7 @@ GLfloat gCubeVertexData[216] =
 		i++;
 		if (i == 10)
 			CProfileManager::dumpAll();
-	}
+	}	
 }
 
 
@@ -336,13 +376,13 @@ GLfloat gCubeVertexData[216] =
     
 	// draw the boxes for the dice with effect framework
 	glPushGroupMarkerEXT(0, "Dice");
-	glBindVertexArrayOES(_vertexArray);
+//	glBindVertexArrayOES(_vertexArray);
 
-	float worldMat[16];
+	float objectTransform[16];
 	for (int i = 0; i < self.diceNumber; i++)
 	{
-		sBoxBodies[i]->getCenterOfMassTransform().getOpenGLMatrix(worldMat);
-		GLKMatrix4 objectTransformMatrix = GLKMatrix4MakeWithArray(worldMat);
+		sBoxBodies[i]->getCenterOfMassTransform().getOpenGLMatrix(objectTransform);
+		GLKMatrix4 objectTransformMatrix = GLKMatrix4MakeWithArray(objectTransform);
 		
 		GLKMatrix4 cameraTransformMatrix	= GLKMatrix4MakeTranslation(0.0f, 0.0f, -30.0f);
 		self.effect.transform.modelviewMatrix = GLKMatrix4Multiply(cameraTransformMatrix, objectTransformMatrix);
